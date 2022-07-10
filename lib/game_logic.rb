12face416d8319689e2d7b5293fed8badf328e23
Noak Pettersson <noake12@gmail.com>
2022-07-10 21:02:33 +0200

    exacts method works

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -1,13 +1,30 @@
 # frozen_string_literal: false
 
-# Modue for the logic
+# Module for guess controls
 module GameLogic
-  def play(part)
-    {
-      1 => human,
-      2 => computer
-    }[part]
+  def find_clues(code, guess)
+    @code_clone = code.clone
+    @guess_clone = guess.clone
   end
 
-  def display_guess; end
+  def exacts(code, guess)
+    @exacts = 0
+    code.each_with_index do |v, i|
+      next unless v == guess[i]
+
+      @exacts += 1
+      code[i] = '*'
+      guess[i] = '*'
+    end
+    @exacts
+  end
+
+  def color(code, guess); end
 end
+
+include GameLogic
+
+a = [1, 2, 1, 4]
+b = [1, 1, 1, 5]
+puts exacts(a, b)
+puts
