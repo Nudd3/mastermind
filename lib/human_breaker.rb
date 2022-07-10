0de71df79094ec66c97bd59bc95aba64c9a68a1b
Noak Pettersson <noake12@gmail.com>
2022-07-10 16:06:55 +0200

    added files for human breaker and computer breaker

---
+++ lib/human_breaker.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: false
+
+# Class representing when the human is the breaker
+class HumanBreaker
+  # include GameLogic
+
+  def initialize
+    create_code
+    play(1)
+  end
+
+  private
+
+  def create_code
+    @code = 4.times.map { rand(1..6) }
+  end
+
+  def play; end
+end
