35c6a08d416d0988130b563113fb4d22123c8567
Noak Pettersson <noake12@gmail.com>
2022-07-11 16:35:18 +0200

    Clues are generated as intended

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -5,26 +5,45 @@ module GameLogic
   def find_clues(code, guess)
     @code_clone = code.clone
     @guess_clone = guess.clone
+    @exacts = exacts
+    @colors = color
+    {
+      1 => @exacts,
+      2 => @colors
+    }
   end
 
-  def exacts(code, guess)
-    @exacts = 0
-    code.each_with_index do |v, i|
-      next unless v == guess[i]
+  def exacts
+    exacts = 0
+    @code_clone.each_with_index do |v, i|
+      next unless v == @guess_clone[i]
 
-      @exacts += 1
-      code[i] = '*'
-      guess[i] = '*'
+      exacts += 1
+      @code_clone[i] = '*'
+      @guess_clone[i] = '*'
     end
-    @exacts
+    exacts
   end
 
-  def color(code, guess); end
+  def color
+    color = 0
+    @code_clone.each_with_index do |v, i|
+      @guess_clone.each_with_index do |e, j|
+        next if e.is_a?(String) || i == j
+
+        next unless v == e
+
+        color += 1
+        @code_clone[i] = '?'
+        @guess_clone[j] = '?'
+      end
+    end
+    color
+  end
 end
 
 include GameLogic
 
-a = [1, 2, 1, 4]
+a = [1, 2, 1, 1]
 b = [1, 1, 1, 5]
-puts exacts(a, b)
-puts
+puts find_clues(a, b)
