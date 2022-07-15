2d87c3f8a836c09d81dcb79d36a4f08e9d95c4f7
Noak Pettersson <noake12@gmail.com>
2022-07-15 17:50:25 +0200

    .

--- lib/computer_breaker.rb
+++ lib/computer_breaker.rb
@@ -1,26 +1,44 @@
 # frozen_string_literal: false
 
+require_relative 'display'
+require_relative 'messages'
+require_relative 'game_logic'
+
 # Class for when computer is breaking the code
 class ComputerBreaker
+  include GameLogic
+  include Messages
+  include Display
+
   def initialize
     @guesses = 12
-    create_code
+    @code = create_code
     play
   end
 
   def create_code
-    2.times do
+    loop do
       print "
   Create your code
   =>
   "
       input = gets.chomp.split(//).map(&:to_i)
-      # break if code_valid?(input)
-      break if input_valid?(input)
+
+      return input if input_valid?(input)
     end
   end
 
-  def play; end
+  def play
+    until @guesses.zero?
+      guess = computer_guess
+      display_turn(@code, guess)
+      break if winner?(@code, guess)
 
-  def solve; end
+      @guesses -= 1
+    end
+  end
+
+  def computer_guess
+    4.times.map { rand(1..6) }
+  end
 end
